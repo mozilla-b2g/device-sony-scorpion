caab9805a67d4a014cf53e26c4a2a5cd8d399dce
Adam Farden <adam@farden.cz>
2016-02-09 21:00:58 +0100

    Add config to enable entry to recovery
    Signed-off-by: Adam Farden <adam@farden.cz>

--- scorpion.mk
+++ scorpion.mk
@@ -21,4 +21,7 @@ PRODUCT_MODEL := Xperia Z3 Tablet Compact (B2G)
 GAIA_DEV_PIXELS_PER_PX := 2
 BOOTANIMATION_ASSET_SIZE := 1080p
 
+PRODUCT_COPY_FILES += \
+    device/sony/scorpion/bootrec-device:root/sbin/bootrec-device
+
 $(call inherit-product-if-exists, vendor/sony/scorpion-blobs/scorpion-vendor-blobs.mk)
